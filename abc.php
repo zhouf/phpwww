874f881f8183a345acf5cb455a1e7161509d0f34
feng zhou <zhoufeng_cq@outlook.com>
2019-05-21 17:15:15 +0800

    改了标点符号

--- abc.php
+++ abc.php
@@ -4,10 +4,10 @@
 
 <?php 
 $zhang = 180;
-echo "Hello",$zhang;
-echo 'this is phpwww<br>'
+echo "Hello",$zhang,'<br>';
+echo 'this is phpwww<br>';
 echo '<h2>kkkkk222</h2>';
-?> 
+?>
 
 </body> 
 </html>
