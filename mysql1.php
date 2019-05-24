8af1b2b7c91725fe42d0bc6ef2604b7101f08825
feng zhou <zhoufeng_cq@outlook.com>
2019-05-24 11:15:48 +0800

    modify mysql1

--- mysql1.php
+++ mysql1.php
@@ -8,29 +8,46 @@ echo 'MySQL<br>',PHP_EOL;
 echo '<br>';
 
 $servername = '192.168.209.60';
-
+$username = 'admin';
+$password = 'AdminKey';
+$db = 'jsp';
 echo $servername;
 
 //创建连接
-$conn = mysqli_connect($servername,'admin','AdminKey','jsp');
-echo $conn;
+$conn = mysqli_connect($servername,$username,$password,$db);
 
 //检查连接
 if(!$conn){
     echo 'NO';
     die('连接失败：'. mysqli_connect_error());
 }
-echo '连接成功';
+echo '连接成功<br>',PHP_EOL;
+
 $result = mysqli_query($conn,'select * from users');
 if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
-        echo 'stunum:',$row['stunum'],'  username:',$row['username'],'<br>';
+        echo $row['stunum'],' => ',$row['username'],'<br>';
     }
 }else{
     echo '0 rows';
 }
 mysqli_close($conn);
 
+echo '<br><hr><h2>第二种方式</h2>',PHP_EOL;
+$conn = new mysqli($servername,$username,$password,$db);
+if($conn->connect_error){
+    die('connect error:'.$conn->connect_error);
+}
+$result = $conn->query('select * from users');
+if($result->num_rows>0){
+    while($row = $result->fetch_object()){
+        echo $row->stunum,' --> ',$row->username,'<br>',PHP_EOL;
+    }
+}else{
+    echo 'No rows';
+}
+$conn->close();
+echo 'connect OK',PHP_EOL;
 
 ?>
 </body> 
